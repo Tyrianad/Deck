a016c48a98fac247bea9f1b08b85b85c12744e8d
Francesco Orio <orioeffe@gmail.com>
2020-04-16 11:32:23 +0200

    edited scope, added draw and drawN

--- deck.go
+++ deck.go
@@ -2,23 +2,24 @@ package main
 
 import (
 	"math/rand"
+	"time"
 )
 
-//Deck rappresenta un mazzo di carte
-type Deck struct {
-	cards []Card
+//deck rappresenta un mazzo di carte
+type deck struct {
+	cards []card
 }
 
-func (d Deck) printAll() {
+func (d deck) printAll() {
 	for _, card := range d.cards {
 		card.println()
 	}
 }
 
-func (d *Deck) shuffle() *Deck {
-	var shuffledDeck Deck
+func (d *deck) shuffle() *deck {
+	var shuffledDeck deck
 	var j int
-	r := rand.New(rand.NewSource(99))
+	r := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
 
 	totalCards := len(d.cards)
 
@@ -34,20 +35,36 @@ func (d *Deck) shuffle() *Deck {
 }
 
 //AddCard permette di aggiungere una carta al mazzo
-func (d *Deck) AddCard(card Card) []Card {
+func (d *deck) AddCard(card card) []card {
 	d.cards = append(d.cards, card)
 	return d.cards
 }
 
-func (d *Deck) newDeck() *Deck {
+func (d *deck) newDeck() *deck {
 	suits := []string{"♤", "♡", "♢", "♧"}
 	values := []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"}
 
 	for _, suit := range suits {
 		for _, value := range values {
-			d.AddCard(Card{suit, value})
+			d.AddCard(card{suit, value})
 		}
 	}
 
 	return d
+}
+
+func (d *deck) draw() card {
+	//@TODO gestire il draw su deck vuoto
+	c := d.cards[0]
+	d.cards = d.cards[1:]
+
+	return c
+}
+
+func (d *deck) drawN(nCards int) []card {
+	//@TODO gestire il drawN su deck con meno carte
+	c := d.cards[:nCards]
+	d.cards = d.cards[nCards:]
+
+	return c
 }
